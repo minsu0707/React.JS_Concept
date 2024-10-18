c36dacb44789f927ee96921f2b2c0c217abdcee3
minsu0707 <mandoo4137@gmail.com>
2024-10-18 20:28:34 +0900

    ADD :: add todo

--- todolist-legend/src/App.jsx
+++ todolist-legend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 import { Header } from "./components/Header";
 import { Editor } from "./components/Editor";
@@ -27,11 +27,22 @@ const mockData = [
 
 function App() {
   const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+    setTodos([newTodo, ...todos]);
+  };
 
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
