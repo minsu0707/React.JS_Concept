10e7e765633b13260e2d3ff8dfba7fdba09a76af
minsu0707 <mandoo4137@gmail.com>
2024-10-18 20:01:09 +0900

    App.jsx setting

--- todolist-legend/src/App.jsx
+++ todolist-legend/src/App.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 import { Header } from "./components/Header";
 import { Editor } from "./components/Editor";
 import { List } from "./components/List";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "리액트 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
+
   return (
     <div className="App">
       <Header />
